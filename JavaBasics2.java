cff20c42356fb5554ee10c11f35690fe449d01ee
Rakat-27 <syedrafiulislam627@example.com>
2025-08-31 15:42:40 +0530

    added new helper function to calculate binomial coefficient

--- JavaBasics2.java
+++ JavaBasics2.java
@@ -20,14 +20,22 @@ public class JavaBasics2 {
         }
         return fact;
     }
+
+    public static int bincoeff(int n,int r){
+        int fact_n=factorial(n);
+        int fact_r=factorial(r);
+        int fact_n_r=factorial(n-r);
+        return fact_n/(fact_r*fact_n_r);
+    }
     public static void main(String[] args) {
         
         // print();
         // calcsum(5,6);
         // int result=multiply(7,7 );
         // System.out.println(result);
-        int factResult=factorial(5);
-        System.out.println(factResult);
+        // int factResult=factorial(5);
+        // System.out.println(factResult);
+        System.out.println(bincoeff(5,2));
     }
     
 }
