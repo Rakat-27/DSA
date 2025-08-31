fa864450dbbe9d6741c3c83b592e54eb353dd669
Rakat-27 <syedrafiulislam627@example.com>
2025-08-31 23:09:23 +0530

    arrays creation

---
+++ ArrayCC.java
@@ -0,0 +1,10 @@
+public class ArrayCC {
+    public static void main(String[] args) {
+        int marks[]=new int[12]; //static size means not changed
+
+        int numbers[]={1,2,3,4};
+        String fruits[]={"apple","mango","banana"};
+
+    }
+    
+}
