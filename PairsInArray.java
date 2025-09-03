2186c78d05b73de83a29d04ddedb161f59db2bbc
Rakat-27 <syedrafiulislam627@example.com>
2025-09-03 15:57:31 +0530

    another approach for count the pairs

--- PairsInArray.java
+++ PairsInArray.java
@@ -1,20 +1,28 @@
 public class PairsInArray {
 
-    public static void printPairs(int num[]){
-        int total_pairs=0;
-        for (int i=0;i<num.length;i++){
-            int curr=num[i];
-            for(int j=i+1;j<num.length;j++){
-                System.out.print("("+curr+","+num[j]+") ");
-                total_pairs++;
-            }
-            System.out.println();
-        }
+    // public static void printPairs(int num[]){
+    //     int total_pairs=0;
+    //     for (int i=0;i<num.length;i++){
+    //         int curr=num[i];
+    //         for(int j=i+1;j<num.length;j++){
+    //             System.out.print("("+curr+","+num[j]+") ");
+    //             total_pairs++;
+    //         }
+    //         System.out.println();
+    //     }
+    //     System.out.println("Total Pairs: "+total_pairs);
+    // }
+
+    public static void countPairs(int n){
+        int total_pairs=n*(n-1)/2;
         System.out.println("Total Pairs: "+total_pairs);
+         
     }
     public static void main(String[] args) {
         int num[]={2,4,6,8,10};
-        printPairs(num);
+        // printPairs(num);
+        int n=num.length;
+        countPairs(n);
     }
     
 }
