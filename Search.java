870d4708f8bd9476ff9d0c1190c0891159fbe0fe
Rakat-27 <syedrafiulislam627@example.com>
2025-09-03 13:34:44 +0530

    item found in menu arrays...it is a string array

--- Search.java
+++ Search.java
@@ -1,22 +1,29 @@
 public class Search {
 
-    public static int linearSearch(int numbers[],int key){
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i]==key){
+    public static int linearSearch(String menu[],String item){
+        for(int i=0;i<menu.length;i++){
+            if(menu[i]==item){
                 return i;
             }
         }
         return -1;
     }
+
+
+
+
+
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10,12,14,16};
-        int key=1;
-        int index=linearSearch(numbers, key);
+        String menu[]={"coffee","tea","samosa","pasta","burger"};
+        String item="pasta";
+        
+        int index=linearSearch(menu,item);
         if(index==-1){
             System.out.println("Not found");
         }
         else{
-            System.out.println("Key found at index: "+index);
+            System.out.println("item found at index: "+index);
         }
          
     
