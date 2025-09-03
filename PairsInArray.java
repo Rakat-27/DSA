509c30efd235bb24641255d5b847b151c0b7b537
Rakat-27 <syedrafiulislam627@example.com>
2025-09-03 15:33:15 +0530

    total pairs count code added

--- PairsInArray.java
+++ PairsInArray.java
@@ -1,13 +1,16 @@
 public class PairsInArray {
 
     public static void printPairs(int num[]){
+        int total_pairs=0;
         for (int i=0;i<num.length;i++){
             int curr=num[i];
             for(int j=i+1;j<num.length;j++){
                 System.out.print("("+curr+","+num[j]+") ");
+                total_pairs++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs: "+total_pairs);
     }
     public static void main(String[] args) {
         int num[]={2,4,6,8,10};
