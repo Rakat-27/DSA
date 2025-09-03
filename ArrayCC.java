3c44279b0c8cfdf0525f0711f40ab830987fe579
Rakat-27 <syedrafiulislam627@example.com>
2025-09-03 13:17:59 +0530

    arrays is passing as pass by reference

--- ArrayCC.java
+++ ArrayCC.java
@@ -1,9 +1,33 @@
-public class ArrayCC {
-    public static void main(String[] args) {
-        int marks[]=new int[12]; //static size means not changed
+import java.util.Scanner;
 
-        int numbers[]={1,2,3,4};
-        String fruits[]={"apple","mango","banana"};
+public class ArrayCC {
+
+    //pass by reference
+
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+3;
+        }
+    }
+    public static void main(String[] args) {
+        // int marks[]=new int[12]; //static size means not changed
+
+        // int numbers[]={1,2,3,4};
+        // String fruits[]={"apple","mango","banana"};
+
+        // int marks[]=new int[100];
+        // Scanner sc=new Scanner(System.in);
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+        // marks[2]=sc.nextInt();
+
+        // System.out.println(marks[0]);
+        int marks[]={97,98,99};
+        update(marks);
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+        
 
     }
     
