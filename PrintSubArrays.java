828b3ee29d1440bd0dfeff3a8336e4023c313e90
Rakat-27 <syedrafiulislam627@example.com>
2025-09-07 04:17:24 +0530

    count subarrays also

--- PrintSubArrays.java
+++ PrintSubArrays.java
@@ -2,6 +2,7 @@ public class PrintSubArrays {
 
     public static void printSubArrays(int numbers[]){
         int n=numbers.length;
+        int total_subarrays=0;
         for(int i=0;i<n;i++){
             int start=i;
             for(int j=i;j<n;j++){
@@ -9,10 +10,12 @@ public class PrintSubArrays {
                 for(int k=start;k<=end;k++){
                     System.out.print(numbers[k]+" ");
                 }
+                total_subarrays++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total subarrays = "+total_subarrays);
         
     }
     public static void main(String[] args) {
