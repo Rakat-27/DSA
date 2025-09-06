9ec1c59fff3a7d3b1f8efe87899c9c759c3946d9
Rakat-27 <syedrafiulislam627@example.com>
2025-09-07 04:19:54 +0530

    count subarrays using formula

---
+++ CountSubArrays.java
@@ -0,0 +1,10 @@
+public class CountSubArrays {
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        int n=arr.length;
+        int total_subarrays=n*(n+1)/2;
+        System.out.println("Total subarrays = "+total_subarrays);
+        
+    }
+    
+}
